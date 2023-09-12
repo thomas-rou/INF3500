16bbba4dd1d809521c51eabb6d0bad870ecfbf17
ThomasR <thomas.rouleau@polymtl.ca>
2023-09-12 11:00:03 -0400

    demo_combinatoire: fin de la partie 1
    Partie 1 du labo complété (sections a à j) et testé

--- TP1/sources/demo_combinatoire.vhd
+++ TP1/sources/demo_combinatoire.vhd
@@ -24,29 +24,29 @@ port (
     pair: out std_logic;        -- indique si le nombre est pair
     divpar4 : out std_logic;    -- indique si le nombre est divisible par 4
     divpar5 : out std_logic;    -- indique si le nombre est divisible par 5
-    divpar8 : out std_logic;     -- indique si le nombre est divisible par 8
-);
+    divpar8 : out std_logic     -- indique si le nombre est divisible par 8
+); --";" retiré d'arpès la définition de divpar8 car il s'agit du dernier identificateur
 end demo_combinatoire;
 
 
 architecture arch1 of demo_combinatoire is
 begin                          
     
-    pair <= not(A(1);
+    pair <= not(A(0)); --Ajout d'une parenthèse manquante, parité doit être évalué avec le bit le moins significatif (A(1) -> A(0))
     
     divpar4 <= (not(A(3)) and not(A(2)) and not (A(1)) and not(A(0))) -- m0
     or (not(A(3)) and (A(2)) and not(A(1)) and not(A(0))) -- m4
-    or ((A(3)) and not(A(2)) and not(A(1)) and not(A(0)) -- m8
-    or (not(A(3)) and not(A(2)) and (A(1)) and (A(0))); -- m12
+    or ((A(3)) and not(A(2)) and not(A(1)) and not(A(0))) -- m8 (Ajout d'une parenthèse manquante)
+    or ((A(3)) and (A(2)) and not(A(1)) and not(A(0))) ; -- m12	(12 = 1100 et non 0011 qui = 3)
     
     with to_integer(A) select
     divpar5 <=
         '1' when 0,
-        '1' when 6,
+        '1' when 5,	-- 6 n'est pas divisible par 5, 5 l'est
         '1' when 10,
-        '1' when 15
+        '1' when 15, --Ajout d'une virgule manquante
         '0' when others;
         
-    divpar8 <= '1' when A = "1000" or A = 0001 else '0';
+    divpar8 <= '1' when A = "1000" or A = 0000 else '0'; -- 1 n'est pas divisible par 8, 0 l'est
     
-end arch;
+end arch1; --Correction du label de fin de l'architecture pour arch1(était arch avant)
