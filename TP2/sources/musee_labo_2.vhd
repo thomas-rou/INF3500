1c8b8bfd15d102aa708e3395c14b1723a5a68b90
ThomasR <thomas.rouleau@polymtl.ca>
2023-09-26 11:18:17 -0400

    partie 1 et 2 done

--- TP2/sources/musee_labo_2.vhd
+++ TP2/sources/musee_labo_2.vhd
@@ -33,10 +33,29 @@ begin
     assert N mod 4 = 0 report "Cette architecture ne fonctionne que pour N muliple de 4." severity error;
 
     -- code à modifier pour la partie 1
-    alarme_intrus <= '1';
-    alarme_jasette <= '1';
-    alarme_greve <= '1';
-    alarme_generale <= '1';
+	process(detecteurs_mouvement)
+	variable compte_detecteur_actif : natural := 0;
+	begin
+		compte_detecteur_actif 	:= 0;
+		alarme_intrus 			<= '0';
+		alarme_jasette 			<= '0';
+		alarme_greve 			<= '0';
+		alarme_generale 		<= '0';
+		for k in N - 1 downto 0 loop
+			if detecteurs_mouvement(k) = '1' then
+				compte_detecteur_actif := compte_detecteur_actif + 1;
+			end if;
+		end loop;
+		if 	compte_detecteur_actif = 0 then
+			alarme_greve <= '1'; -- aucun détecteur de mouvement n'est actif
+		elsif compte_detecteur_actif <= 2 then
+			alarme_jasette <= '1'; -- un ou deux détecteur de mouvement sont actifs
+		elsif compte_detecteur_actif <= (N-1) and compte_detecteur_actif /= 3 then
+			alarme_intrus <= '1'; -- au moins 4 détecteur de mvmt actif
+		elsif compte_detecteur_actif = N then
+			alarme_generale <= '1'; -- tous les détecteurs sont activés
+		end if;
+	end process;
     
     -- code à modifier pour le bonus de la partie 4a.
     alarme_reserve <= '1';
--- TP2/sources/musee_labo_2_tb.vhd
+++ TP2/sources/musee_labo_2_tb.vhd
@@ -13,7 +13,7 @@ use work.all;
 
 entity musee_labo_2_tb is
     generic (
-        N_tb : positive := 8
+        N_tb : positive := 16
     );
 end entity;
 
@@ -44,15 +44,36 @@ begin
     
     -- application exhaustive des vecteurs de test, affichage et vérification
     process
-    
+	variable compte_tb : natural := 0;
     begin
-
+		compte_tb := 0;
         for k in 0 to 2 ** N_tb - 1 loop
 
             detecteurs_mouvement_tb <= std_logic_vector(to_unsigned(k, N_tb));
 
-            wait for 10 ns; -- nécessaire pour que les signaux se propagent dans l'UUT
-            
+            wait for 10 ns; -- nécessaire pour que les signaux se propagent dans l'UUT	
+			
+			compte_tb := 0;	
+			
+			for k in N_tb - 1 downto 0 loop
+				if detecteurs_mouvement_tb(k) = '1' then
+				compte_tb := compte_tb + 1;
+				end if;
+			end loop;
+		  	
+			if compte_tb = 0 then				
+				assert(alarme_greve_tb = '1')		report "erreur grève" 		severity warning;
+				assert(alarme_jasette_tb = '0' or alarme_intrus_tb = '0' or alarme_generale_tb = '0')	report "Plusieurs alarmes" 	severity warning;
+			elsif compte_tb <= 2 then
+				assert(alarme_jasette_tb = '1')		report "erreur jasette" 	severity warning;
+				assert(alarme_greve_tb = '0' or alarme_intrus_tb = '0' or alarme_generale_tb = '0')		report "Plusieurs alarmes" 	severity warning;
+			elsif compte_tb >= 4 and compte_tb < N_tb then
+				assert(alarme_intrus_tb = '1')		report "erreur intrus" 		severity warning;
+				assert(alarme_jasette_tb = '0' or alarme_greve_tb = '0' or alarme_generale_tb = '0')	report "Plusieurs alarmes" 	severity warning;
+			elsif compte_tb = N_tb then
+				assert(alarme_generale_tb = '1')	report "erreur générale" 	severity warning;
+				assert(alarme_jasette_tb = '0' or alarme_intrus_tb = '0' or alarme_greve_tb = '0')		report "Plusieurs alarmes" 	severity warning;
+            end if;
         end loop;
             
         report "simulation terminée" severity failure;
