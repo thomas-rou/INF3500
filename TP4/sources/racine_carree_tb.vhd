ff0f95b5d9904298fdeb6506ba12293cb169e3f4
Mael <michaudmael@gmail.com>
2023-11-01 15:53:43 -0500

    Test exhaustif sans erreur

--- TP4/sources/racine_carree_tb.vhd
+++ TP4/sources/racine_carree_tb.vhd
@@ -39,9 +39,23 @@ begin
 
     clk <= not clk after periode / 2;
     reset <= '1' after 0 ns, '0' after 5 * periode / 4;
+	
+	PROC_SEQUENCE : process
+	begin
+		wait for 5 * periode / 4;
+		for i in 0 to 2**A'length - 1 loop
+			A <= to_unsigned(i, A'length);
+			go <= '1';
+			wait for 10 ns;
+			go <= '0';
+			wait for 110 ns;
+		end loop;
+	assert false report "Test: OK" severity failure;	
+		
+	end process;
 
-    A <= to_unsigned(30000, A'length);                          -- une stimulation de base
-    go <= '0' after 0 ns, '1' after 27 ns, '0' after 37 ns;     -- une stimulation de base
+    --A <= to_unsigned(30000, A'length) after 0 ns, to_unsigned(6755, A'length) after 136 ns;                          -- une stimulation de base
+    --go <= '0' after 0 ns, '1' after 27 ns, '0' after 37 ns, '1' after 136 ns, '0' after 146 ns;     -- une stimulation de base
     
     -- votre code ici
 
